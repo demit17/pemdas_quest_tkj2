517a7da11af8114ec65a7b658b9c2f979ae899e8
demit17 <111479972+demit17@users.noreply.github.com>
2022-08-21 23:25:46 -0700

    Add files via upload

---
+++ python_Dhafin Nafis.py
@@ -0,0 +1,27 @@
+n=("masukan nama anda: ")
+b=("masukan nama belakang: ")
+print("nama anda adalah",n,b)
+a =int(input("masukan nilai a: "))
+b =float(input("masukan nilai b: "))
+c=a+b
+
+print("a=", a)
+print("b=", b)
+print(a,"+",b,"=",c)
+
+x =int(input("masukan nilai mata uang: "))
+y =(9/5 * x) + 32
+print(x,"celcius =",y,"farenheit")
+
+print("3.segitiga")
+
+panjang= int(input("masukan nilai panjang: "))
+lebar= int(input("masukan nilai lebar: "))
+luas_pp= panjang * lebar
+
+print("luas persegi panjang adalah: ", luas_pp)
+
+alas= int(input("masukan nilai alas: "))
+tinggi= int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi /2 
+print ("luas segitiga adalah", luas_s)
